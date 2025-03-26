7679090032d844a55c7b6c48f61cd9eff6030b4a
NoahDelgado <noah.delgado1@eduvaud.ch>
2025-03-26 23:36:43 +0100

    Update courses model, controller and views

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,9 @@
 class CoursesController < ApplicationController
   before_action :authenticate_person!
   before_action :set_course, only: %i[ show edit update destroy ]
+  before_action :authorize_course_access, only: %i[ index ]
+  before_action :authorize_course_management, only: %i[ new create edit update destroy ]
+  before_action :process_time_fields, only: %i[ create update ]
 
   # GET /courses or /courses.json
   def index
@@ -18,21 +21,34 @@ class CoursesController < ApplicationController
   # GET /courses/new
   def new
     @course = Course.new
+    @teachers = Teacher.all
+    @subjects = Subject.all
+    @moments = Moment.all
+    @school_classes = SchoolClass.all
   end
 
   # GET /courses/1/edit
   def edit
+    @teachers = Teacher.all
+    @subjects = Subject.all
+    @moments = Moment.all
+    @school_classes = SchoolClass.all
   end
 
   # POST /courses or /courses.json
   def create
     @course = Course.new(course_params)
+    normalize_times(@course)
 
     respond_to do |format|
       if @course.save
         format.html { redirect_to course_url(@course), notice: "Course was successfully created." }
         format.json { render :show, status: :created, location: @course }
       else
+        @teachers = Teacher.all
+        @subjects = Subject.all
+        @moments = Moment.all
+        @school_classes = SchoolClass.all
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @course.errors, status: :unprocessable_entity }
       end
@@ -41,11 +57,18 @@ class CoursesController < ApplicationController
 
   # PATCH/PUT /courses/1 or /courses/1.json
   def update
+    @course.assign_attributes(course_params)
+    normalize_times(@course)
+
     respond_to do |format|
-      if @course.update(course_params)
+      if @course.save
         format.html { redirect_to course_url(@course), notice: "Course was successfully updated." }
         format.json { render :show, status: :ok, location: @course }
       else
+        @teachers = Teacher.all
+        @subjects = Subject.all
+        @moments = Moment.all
+        @school_classes = SchoolClass.all
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @course.errors, status: :unprocessable_entity }
       end
@@ -71,5 +94,48 @@ class CoursesController < ApplicationController
     # Only allow a list of trusted parameters through.
     def course_params
       params.require(:course).permit(:start_at, :end_at, :week_day, :school_class_id, :subject_id, :moment_id, :person_id)
+    end
+
+    # Process time fields to handle time_field inputs
+    def process_time_fields
+      if params[:course][:start_at].present? && params[:course][:start_at].is_a?(String) && !params[:course][:start_at].include?("T")
+        # If the input is just a time (no date part), add today's date
+        time = params[:course][:start_at]
+        params[:course][:start_at] = "#{Time.zone.today} #{time}"
+      end
+
+      if params[:course][:end_at].present? && params[:course][:end_at].is_a?(String) && !params[:course][:end_at].include?("T")
+        # If the input is just a time (no date part), add today's date
+        time = params[:course][:end_at]
+        params[:course][:end_at] = "#{Time.zone.today} #{time}"
+      end
+
+      # Ensure week_day is a weekday (1-5)
+      if params[:course][:week_day].present?
+        day = params[:course][:week_day].to_i
+        unless (1..5).include?(day)
+          params[:course][:week_day] = 1 # Default to Monday if invalid
+        end
+      end
+    end
+
+    # Remove seconds and milliseconds from time fields
+    def normalize_times(course)
+      course.start_at = course.start_at.change(sec: 0, usec: 0) if course.start_at
+      course.end_at = course.end_at.change(sec: 0, usec: 0) if course.end_at
+    end
+
+    # Ensure proper authorization for course access
+    def authorize_course_access
+      unless current_person.is_a?(Teacher) || current_person.is_a?(Dean)
+        redirect_to root_path, alert: "You are not authorized to access courses."
+      end
+    end
+
+    # Ensure only deans can manage courses
+    def authorize_course_management
+      unless current_person.is_a?(Dean)
+        redirect_to courses_path, alert: "Only deans can create or modify courses."
+      end
     end
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,8 +1,16 @@
 class Course < ApplicationRecord
+  include SoftDeletable
+
   belongs_to :school_class
   belongs_to :subject
   belongs_to :moment
   belongs_to :person
   has_many :examinations
   has_many :sections
+
+  # Weekday scope - only courses on weekdays (Monday to Friday)
+  scope :weekdays, -> { where(week_day: 1..5) }
+
+  # Validation for week_day to ensure it's a weekday
+  validates :week_day, inclusion: { in: 1..5, message: "must be a weekday (Monday to Friday)" }
 end
