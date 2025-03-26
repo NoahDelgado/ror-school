dd775560050a6bbcdd24d03a9b7cc88bac6321cf
NoahDelgado <noah.delgado1@eduvaud.ch>
2025-03-26 23:33:55 +0100

    Update grades model and views

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -1,4 +1,17 @@
 class Grade < ApplicationRecord
+  include SoftDeletable
+
   belongs_to :examination
   belongs_to :person
+
+  validates :value, presence: true,
+                   numericality: {
+                     greater_than_or_equal_to: 1,
+                     less_than_or_equal_to: 6,
+                     step: 0.5
+                   }
+
+  def self.valid_grades
+    (1..6).step(0.5).to_a
+  end
 end
